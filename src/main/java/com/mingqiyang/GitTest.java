7b0dc3568b3a5eef9e3c70420573840eaa7cb61b
mingqi <mingqiyang10@gmail.com>
2022-06-30 00:05:21 +0800

    6.29 second commit

--- src/main/java/com/mingqiyang/GitTest.java
+++ src/main/java/com/mingqiyang/GitTest.java
@@ -3,5 +3,6 @@ package src.main.java.com.mingqiyang;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git！");
+        System.out.println("hello git！");
     }
 }
